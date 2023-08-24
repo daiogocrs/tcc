2c3e34ff30c00bf1cff410769b47725315dd89aa
Diogo Borges Corso <diogobc0702@gmail.com>
2023-08-23 23:40:58 -0300

    asd

--- cardapiologado.php
+++ cardapiologado.php
@@ -31,12 +31,6 @@ $result = $conexao->query($sql);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="Website Icon" type="png" href="fotos/cantinalogo.png">
     <link rel="stylesheet" type="text/css" href="css/cardapio.css">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
@@ -45,6 +39,7 @@ $result = $conexao->query($sql);
     <link href="https://fonts.googleapis.com/css?family=Lora" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
     <title>Tela de Cardápio</title>
 </head>
 
@@ -52,8 +47,6 @@ $result = $conexao->query($sql);
     <header>
         <a href="homelogado.php"><img src="fotos/cantinalogo2.png" alt="logo cantina Federal"></a>
     </header>
-
-    <script type="text/javascript" src="js/cardapio.js"></script>
     <section class="bg-cardapio bg-section" id="cardapio">
         <div class="container-fluid">
             <h1 class="container-h1">Cardápio</h1>
@@ -66,36 +59,43 @@ $result = $conexao->query($sql);
                     <?php } ?>
                 </ul>
                 <div class="tab-content slideanim">
-                    <?php foreach ($categorias as $categoria) { ?>
-                        <div id="<?= $categoria ?>" class="tab-pane fade <?php if ($categoria === 'salgados') echo 'show active'; ?>">
-                            <div class="row">
-                                <?php
-                                $categoria_result = getProdutosByCategoria($conexao, $categoria);
-                                echo '<div class="col-sm-7">';
-                                echo '<ul class="list-group">';
-                                while ($user_data = mysqli_fetch_assoc($categoria_result)) {
-                                    echo '<li class="list-group-item">';
-                                    echo '<h4 class="list-group-item-heading">' . $user_data['nome'];
-                                    echo '<span class="badge pull-right">R$' . $user_data['preco'] . '</span>';
-                                    echo '</h4>';
-                                    echo '</li>';
-                                }
-                                echo '</ul>';
-                                echo '</div>';
-                                ?>
-                                <div class="col-sm-5">
-                                    <div class="right-cover">
-                                        <h3><?php echo ucfirst($categoria); ?></h3>
-                                        <img src="fotos/<?= $categoria ?>.jpg?v=<?= time() ?>" class="cardapio-img img-fluid" alt="Imagem de <?= $categoria ?>">
-                                    </div>
+                <?php foreach ($categorias as $categoria) { ?>
+                    <div id="<?= $categoria ?>" class="tab-pane fade <?php if ($categoria === 'salgados') echo 'show active'; ?> slide">
+                        <div class="row">
+                            <?php
+                            $categoria_result = getProdutosByCategoria($conexao, $categoria);
+                            echo '<div class="col-sm-7">';
+                            echo '<ul class="list-group">';
+                            while ($user_data = mysqli_fetch_assoc($categoria_result)) {
+                                echo '<li class="list-group-item">';
+                                echo '<h4 class="list-group-item-heading">' . $user_data['nome'];
+                                echo '<span class="badge pull-right">R$' . $user_data['preco'] . '</span>';
+                                echo '</h4>';
+                                echo '</li>';
+                            }
+                            echo '</ul>';
+                            echo '</div>';
+                            ?>
+                            <div class="col-sm-5">
+                                <div class="right-cover">
+                                    <h3><?php echo ucfirst($categoria); ?></h3>
+                                    <img src="fotos/<?= $categoria ?>.jpg?v=<?= time() ?>" class="cardapio-img img-fluid" alt="Imagem de <?= $categoria ?>">
                                 </div>
                             </div>
                         </div>
-                    <?php } ?>
-                </div>
+                    </div>
+                <?php } ?>
+            </div>
             </div>
         </div>
     </section>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
 </body>
 
 
