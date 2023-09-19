17b50ef26e67f6ea0160097de789eca7ea6964f9
Diogo Borges Corso <diogobc0702@gmail.com>
2023-09-19 09:12:52 -0300

    asd

--- homeusuario/homeusuario.php
+++ homeusuario/homeusuario.php
@@ -364,7 +364,7 @@ $result = $conexao->query($sql);
         <div class="container-fluid">
             <div class="row footer-align">
                 <div class="col-md-6 col-sm-6">
-                    <h5>Our contact</h5>
+                    <h5>Nosso Contato</h5>
                     <hr class="hr-foot">
                     <div class="footer-items">
                         <address>
