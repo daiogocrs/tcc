b736d329d86143896214c2a2377c364702f7c185
Diogo Borges Corso <diogobc0702@gmail.com>
2023-10-27 10:58:34 -0300

    arrumando css

--- config.php
+++ config.php
@@ -1,9 +1,14 @@
 <?php
 
-    $dbHost = 'localhost';
+    /*$dbHost = 'localhost';
     $dbUsername = 'id21461309_cantinafederal';
     $dbPassword = '@Diogo0702';
-    $dbName = 'id21461309_bdcantina';
+    $dbName = 'id21461309_bdcantina';*/
+
+    $dbHost = 'localhost';
+    $dbUsername = 'root';
+    $dbPassword = '';
+    $dbName = 'bdcantina';
     
     $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
 
